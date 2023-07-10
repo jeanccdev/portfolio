dfad8642849a3e77ba838310e33874fc91be66b3
omniavincit98 <jeancc1532@gmail.com>
2023-07-10 16:49:19 -0300

    Adjusting images path

--- src/routes/Projects.tsx
+++ src/routes/Projects.tsx
@@ -1,15 +1,18 @@
-const Projects = () =>{
+import jaquelinePreview from '../public/jaquelinePreview2.png'
+import shayanePreview from '../public/shayanepreview.png'
+
+const Projects = () => {
     return (
         <>
             <section id="projects">
                 <h2>Projetos</h2>
                 <div>
                     <div>
-                        <a href="https://jaquelinecristovao.com/" target="_blank" rel="noopener noreferrer"><img src="../public/jaquelinePreview2.png" alt="Jaqueline Cristóvão" /></a>
+                        <a href="https://jaquelinecristovao.com/" target="_blank" rel="noopener noreferrer"><img src={jaquelinePreview} alt="Jaqueline Cristóvão" /></a>
                         <p>PHP e MySQL</p>
                     </div>
                     <div>
-                        <a href="https://shayanezonta.000webhostapp.com/" target="_blank" rel="noopener noreferrer"><img src="../public/shayanepreview.png" alt="Shayane Zonta Beauty" /></a>
+                        <a href="https://shayanezonta.000webhostapp.com/" target="_blank" rel="noopener noreferrer"><img src={shayanePreview} alt="Shayane Zonta Beauty" /></a>
                         <p>PHP e MySQL</p>
                     </div>
                     <div>
